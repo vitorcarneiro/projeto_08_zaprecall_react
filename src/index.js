fcc50849595c409223821096aa1ec669bb54259d
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2021-12-11 10:33:50 -0300

    css part 2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './css/all-screens.css';
 
 import logoMini from './img/logo-mini.png';
+import turn from './img/turn.png';
+
+let page = 1;
+const totalPages = 8;
 
 function App() {
     return (
@@ -11,7 +15,22 @@ function App() {
         <header>
             <img src={logoMini} />
         </header>
-        <main className='card'></main>
+
+        <main className='card'>
+            <p className='page-marker'>{page}/{totalPages}</p>
+            
+
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non ullamcorper at quis eu. Malesuada iaculis viverra a tincidunt arcu nullam. Orci tortor arcu placerat id sit et. Elementum in erat cras tortor at auctor diam.</h2>
+            
+            <div className="bottom-card-boxes">
+                <p className="bottom-box black">Aprendi agora</p>
+                <p className="bottom-box red">Não lembrei</p>
+                <p className="bottom-box green">Lembrei com esforço</p>
+                <p className="bottom-box yellow">Zap!</p>
+            </div>
+
+            {/* <img src={turn} className='turn'/> */}
+        </main>
     </>
     
     );
